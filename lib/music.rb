91d3a4250f8a44239482d81e934f0bb39b7f749e
boom100100 <bsd231@nyu.edu>
2020-03-29 14:14:59 -0400

    Added playlist functionality.

--- lib/music.rb
+++ lib/music.rb
@@ -1,15 +1,42 @@
-require 'nokogiri'
-require 'open-uri'
 require 'pathname'
 require 'pry'
 
 class Music
-  
-  def self.background_noise_menu
 
+  def self.background_noise_menu
+    Music.build_menu("background_noise")
   end
 
   def self.playlist_menu
+    Music.build_menu("playlist")
+  end
 
+  def self.build_menu(path)
+    puts ''
+    puts 'Choose from the tracks below.'
+    puts 'Enter the number next to the track to play it.'
+    puts 'Enter (a) to play all on shuffle.'
+    puts ''
+    entries = Dir::entries("./db/mp3s/#{path}").drop(2)
+    entries.each_with_index do |title, index|
+      puts "#{index+1}. #{title}"
+    end
+    input = gets.chomp
+    puts ''
+    x = Integer(input) rescue false
+    if x && x.between?(1,entries.length)
+
+      fork { exec("mpg123 -q ./db/mp3s/#{path}/#{entries[x-1]}")}
+    elsif input == 'a'
+      fork { exec("mpg123 -qz ./db/mp3s/#{path}/*")}
+    else
+      puts 'Input is invalid. Please try again.'
+      puts ''
+      Music.build_menu(path)
+    end
+  end
+
+  def self.stop
+    fork { exec("killall mpg123")}
   end
 end
