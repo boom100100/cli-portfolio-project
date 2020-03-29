c6ba9881d53443cfb21c938af4dde849895bb74e
boom100100 <bsd231@nyu.edu>
2020-03-29 14:14:01 -0400

    Added music functionality.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -8,8 +8,8 @@ class CommandLineInterface
     puts ''
     input = ''
     while input != 'exit'
-      puts 'Enter "n" for new game, "t" for tutorial, and "s" for strategy tips.'
-      puts ' Enter "m" to set music options.'
+      puts 'Enter (n) for new game, (t) for tutorial, and (s) for strategy tips.'
+      puts ' Enter (m) to set music options.'
       puts 'Enter "exit" to exit.'
       input = gets.chomp
       process_input(input)
@@ -21,17 +21,26 @@ class CommandLineInterface
     case input
     when "n"
       Game.new.start
+      #self.call
     when "t"
       Game.tutorial
     when "s"
       Game.strategy
     when "m"
-      Scraper.scrape_pages
+      if Dir.empty?('./db/mp3s/background_noise')
+        Scraper.scrape_pages
+      end
       puts ''
       puts 'Do you prefer background noise (b) or casino playlist music (p)?'
+      puts 'You can also stop (s) what is playing.'
       input = gets.chomp
       if input.downcase == 'b'
+        Music.background_noise_menu
       elsif input.downcase == 'p'
+        Music.playlist_menu
+      elsif input.downcase == 's'
+        Music.stop
+        puts ''
       else
         puts ''
         puts 'Input is invalid. Please try again.'
